38d830991c11871cb0fc2977501d8c8b1aca208e
Pharaoh <pharaoh@lindenlab.com>
2016-02-13 14:09:27 -0800

    Initial Commit

---
+++ main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+//const electron = require('electron');
+const app = require('app')
+const usb = require('electron-usb')
+const BrowserWindow = require('browser-window')
+
+
+var mainWindow = null;
+//console.log(electron)
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+//usb.getDeviceList();
+
+app.on('ready', function()  {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+//  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
+
+
