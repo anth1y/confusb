804ae622a63eee4c8d1182b5efb551f5df5ca639
Pharaoh <pharaoh@lindenlab.com>
2016-03-12 14:40:10 -0800

    Updating code to reflect modernity

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-var $ = require('jquery')
-var usb = require('usb-detection')
-var hbars = require('handlebars');
+"use strict"
+const $ = require('jquery')
+const usb = require('usb-detection')
+const hbars = require('handlebars');
 
 
 function renderCheckboxes () {
-    var form = $('#form')
-    var source = $("#template").html()
-    var template = hbars.compile(source)
-    usb.find(function(err, devices) {  
+    let form = $('#form')
+    let source = $("#template").html()
+    let template = hbars.compile(source)
+    usb.find(function(err, devices) {
       if (err) {
         return console.log(err)
       }
 
-      var html = template({ 
+      let html = template({
           devices: devices.filter(function(device){
              return device.manufacturer.indexOf('Apple') === -1
           })
